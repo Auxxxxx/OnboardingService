fd3068eccbe66e531f85f3a28b0ef47c3183713b
NLastya <nlastya1994@mail.ru>
2023-12-28 18:00:53 +0300

    repair navbar 2

---
+++ react-frontend/src/components/innerpages.component.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import Navigation from './navbar.component';
+import NotesList from './noteList.component';
+import MediaList from './mediaList';
+
+const InnerPage = (props) => {
+  const [showNavbar, setShowNavbar] = useState(false);
+  
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
+
+  const [userData, setUserData] = useState([]);
+
+  // useEffect(() => {
+  //   // Выполнение запроса при монтировании компонента
+  //   fetch('http://localhost:8080/note/useful-info')
+  //     .then(response => response.json())
+  //     .then(data => {
+  //        setUserData(data);
+  //     })
+  //     .catch(error => {
+  //       console.error('Ошибка получения данных:', error);
+  //     });
+  // }, []);
+  const userDataList = ['1 строка', 'джакузи', 'одеяло'];
+
+  return (
+
+    <main className = "main inner-main">
+      <div class="inner-div">
+      <img class = "inner-img" src= {props.logoimg} ></img>
+      <h2 class = "pages-h2 inner-h2 " >{props.hed}</h2>
+      <button className ="inner-get-nav" onClick={handleShowNavbar}></button>
+            {showNavbar &&
+             <Navigation showNavbar = {showNavbar} setShowNavbar = {setShowNavbar} />}
+
+      <p class = "pages-p" >{props.p}</p>
+      {/* <ul class = "inner-ul info-ul">
+        {userDataList.map((userString) => (
+          <li class = "inner-li info-li" ><img className = "inner-li-img" src = "info-li.svg" alt ="point"></img>
+          {userString}
+          </li> */}
+        {/* ))}
+      </ul> */}
+    if(props.class === "notes"){
+     <NotesList class = {props.class} list = {props.list}></NotesList>
+    }
+      elif(props.class === "media"){
+      <MediaList class = {props.class} list = {props.list}></MediaList>
+    } else
+       return {null}
+
+      </div>
+    </main>
+  );
+};
+
+export default InnerPage;
