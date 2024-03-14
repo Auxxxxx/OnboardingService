d6768bc1fc4c556b260ca2b53ed6e8ab7bf6c31f
Alex <kriukov_2004@mail.ru>
2024-03-14 16:05:26 +0300

    edited for deployment

--- vue-front/vite.config.js
+++ vue-front/vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 5173
+  },
+  preview: {
+    port: 3030
+  }
 })
